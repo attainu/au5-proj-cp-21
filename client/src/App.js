2bf8882dd79482bbdf80760b514eb71df4308c2c
rajat-jain-au5 <jrajat862@gmail.com>
2020-06-17 22:04:16 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Splash from './component/Splash'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <Splash/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/Splash.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Splash extends React.Component {
+  render() {
+    return (
+      <div
+        className="Icon1"
+        style={{ backgroundColor: "white" }}
+      >
+        <div>
+          <button>As A doctor</button>
+        </div>
+        <div>
+          <button>As A patient</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Splash;
